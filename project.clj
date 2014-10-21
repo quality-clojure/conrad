0c24c39ee82d65ca59ae3475f983491701ee0ef6
Scott Bauer <sbauer322@gmail.com>
2014-10-20 23:33:19 -0400

    Building out initial outline of library.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/quality-clojure/conrad"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.taoensso/timbre "3.3.1"]])
--- src/conrad/core.clj
+++ src/conrad/core.clj
@@ -1,6 +1,38 @@
-(ns conrad.core)
+(ns conrad.core
+  (:require [taoensso.timbre :as timbre :refer [info spy]]
+            [clojure.java.io :as io])
+  (:import java.io.File))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; All should return something like a vector of maps containing
+;; :file, :line, :content that the reporter will use to do various things.
+(defn long-lines
+  ""
+  [file max-line-length]
+  (1))
+
+(defn trailing-whitespace
+  ""
+  [file]
+  (1))
+
+(defn missing-doc-string
+  ""
+  [file]
+  (1))
+
+(defn empty-doc-string
+  ""
+  [file]
+  (1))
+
+(defn check-files
+  ""
+  [files reporter]
+  (doseq [file (filter #(and (.isFile %)
+                             (.endsWith (.getPath %) "clj"))
+                       (file-seq files))]
+    (info (.getPath file))
+    (reporter (long-lines file 80))
+    (reporter (trailing-whitespace))
+    (reporter (missing-doc-string))
+    (reporter (empty-doc-string))))
